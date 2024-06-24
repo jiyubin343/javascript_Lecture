54a9a4bd88782f71d17bad87cf855a8b7b755ee1
jiyubin343 <gdang343@gmail.com>
2024-06-24 15:24:58 +0900

    space invader

--- 게임들/스페이스 인베이더/app.js
+++ 게임들/스페이스 인베이더/app.js
@@ -13,11 +13,16 @@ const stopBtn = document.querySelector("#stopBtn");
 const display = document.querySelector("#display");
 
 let playerLoc = stageSize*(stageSize-2)+7 // 202;
+let invadersLoc = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+    30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+]
+let gameInterval;
 
 function makePlayer() {
     stage[playerLoc].classList.add("player");
 }
-makePlayer()
 
 function movePlayer(e) {
     stage[playerLoc].classList.remove("player");
@@ -32,22 +37,57 @@ function movePlayer(e) {
     stage[playerLoc].classList.add("player");
 }
 
-document.addEventListener("keyup", movePlayer)
-
-let invadersLoc = [
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-    15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-    30, 31, 32, 33, 34, 35, 36, 37, 38, 39
-]
-let gameInterval;
-
 function makeInvader() {
     invadersLoc.forEach(function(invader) {
         stage[invader].classList.add("invader");
     })
 }
-makeInvader();
 
-function moveInvader() {}
+
+function moveInvader() {
+    invadersLoc.forEach(function(invader) {
+        stage[invader].classList.remove("invader");
+    })
+    for(let i=0; i < invadersLoc.length; i++) {
+        invadersLoc[i]++;
+        stage[invadersLoc[i]].classList.add("invader");
+    }
+}
+gameInterval = setInterval(moveInvader, 1000);
 
 interval = setInterval(moveInvader, 1000);
+
+function gameStart() {
+    stage[playerLoc].classList.remove("player");
+    invadersLoc.forEach(function(invader) {
+        stage[invader].classList.remove("invader");
+    });
+    playerLoc = stageSize*(stageSize - 2)+7 //202
+    invadersLoc = [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+        30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+    ]
+    makePlayer();
+    makeInvader();
+    displayStatus();
+    gameInterval = setInterval(moveInvader, 1000);
+    document.addEventListener("keyup",movePlayer);
+    gameRun();
+}
+
+function gameStop() {
+    clearInterval(gameInterval);
+    document.removeEventListener("keyup", movePlayer);
+}
+
+function gameRun() {
+    moveInvader();
+}
+
+function displayStatus() {
+    display.innerText = invadersLoc.length + "/" + invadersLoc.length
+}
+
+startBtn.addEventListener("click", gameStart);
+stopBtn.addEventListener("click",gameStop);
